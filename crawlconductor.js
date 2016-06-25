49f154ec12523aaebf214ae86524ffea1ec662ad
kanimuni <kani_munidasa@vielabs.com>
2016-06-24 21:35:08 -0700

    automated the crawl conductor

--- crawlconductor.js
+++ crawlconductor.js
@@ -7,49 +7,57 @@ const washingtonpostConfig = require('./crawlconfig/washingtonpostconfig.js');
 const saveToMongo = require('./mongoman.js');
 
 
-crawlerEngine(telegraphConfig, function(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  saveToMongo(data);
-  }
-});
-
-crawlerEngine(bbcConfig, function(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  saveToMongo(data);
-  }
-});
+setInterval(function() {
+  crawlerEngine(telegraphConfig, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+    saveToMongo(data);
+    }
+  });
+}, 3600000);
 
 
-crawlerEngine(nprConfig, function(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  saveToMongo(data);
-  }
-});
+setInterval(function() {
+  crawlerEngine(bbcConfig, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    saveToMongo(data);
+    }
+  });
+}, 3600000);
 
-crawlerEngine(nytimesConfig, function(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  saveToMongo(data);
-  }
-});
+setInterval(function() {
+  crawlerEngine(nprConfig, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    saveToMongo(data);
+    }
+  });
+}, 3600000);
 
+setInterval(function() {
+  crawlerEngine(nytimesConfig, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    saveToMongo(data);
+    }
+  });
+}, 3600000);
 
-crawlerEngine(washingtonpostConfig, function(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  saveToMongo(data);
-  }
-});
+setInterval(function() {
+  crawlerEngine(washingtonpostConfig, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    saveToMongo(data);
+    }
+  });
+}, 3600000);
